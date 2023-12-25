fbd0294bb7908bf1ed87c4bdeaf5916c9faddfdd
David Avetikov <mr.avetik@gmail.com>
2023-12-25 13:51:45 +0300

    Дополнил методами

--- service/message_service.go
+++ service/message_service.go
@@ -40,29 +40,69 @@ import (
 var messageSrv MessageService
 
 type MessageService interface {
+	// GetMessageAll
+	// Метод для получения всех сообщений в диалоге
+	GetMessageAll(ctx context.Context, conversationId string) (*model.Page[conversationModel.Message], error)
+
 	// CreateMessage
-	// Метод для создания и отправки сообщения в диалог
+	// Метод для создания и отправки сообщений в диалог
 	CreateMessage(ctx context.Context, conversationId string, message conversationModel.Message) (*conversationModel.Message, error)
 
-	//TODO GetMessageById(ctx context.Context, messageId string, fields string) conversationModel.Message
-	//TODO GetMessageAll(ctx context.Context, conversationId string) model.Page[conversationModel.Message]
-	//TODO UpdateMessage(ctx context.Context, conversationId string, messageId string, text string) conversationModel.Message
-	//TODO DeleteMessageById(ctx context.Context, conversationId string, messageId string)
-	//TODO PinMessage(ctx context.Context, conversationId string, messageId string)
-	//TODO UnpinMessage(ctx context.Context, conversationId string, messageId string)
-	//TODO ReadMessage(ctx context.Context, conversationId string, messageId string)
-	//TODO AddLabelToMessage(ctx context.Context, conversationId string, messageId string, labelId string)
-	//TODO RemoveLabelToMessage(ctx context.Context, conversationId string, messageId string, labelId string)
-	//TODO ReactMessage(ctx context.Context, conversationId string, messageId string, reactionId string)
+	// UpdateMessage
+	// Метод для изменения сообщений в диалоге
+	UpdateMessage(ctx context.Context, conversationId string, messageId string, text string) (*conversationModel.Message, error)
 
-	//TODO GetThreadMessageAll(ctx context.Context, conversationId string, parentMessageId string) conversationModel.Message
-	//TODO CreateThreadMessage(ctx context.Context, conversationId string, parentMessageId string, message conversationModel.Message) conversationModel.Message
-	//TODO UpdateThreadMessage(ctx context.Context, conversationId string, parentMessageId string, messageId string, text string) conversationModel.Message
-	//TODO DeleteThreadMessageById(ctx context.Context, conversationId string, parentMessageId string, messageId string)
-	//TODO ReadThreadMessage(ctx context.Context, conversationId string, parentMessageId string, messageId string)
-	//TODO AddLabelToThreadMessage(ctx context.Context, conversationId string, parentMessageId string, messageId string, labelId string)
-	//TODO RemoveLabelToThreadMessage(ctx context.Context, conversationId string, parentMessageId string, messageId string, labelId string)
-	//TODO ReactThreadMessage(ctx context.Context, conversationId string, parentMessageId string, messageId string, reactionId string)
+	// DeleteMessageById
+	// Метод для удаления сообщений из диалога по ID
+	DeleteMessageById(ctx context.Context, conversationId string, messageId string) error
+
+	// PinMessage
+	// Метод для закрепления сообщений в диалоге по ID
+	PinMessage(ctx context.Context, conversationId string, messageId string) error
+
+	// UnpinMessage
+	// Метод для удаления закрепления сообщения из диалога по ID
+	UnpinMessage(ctx context.Context, conversationId string, messageId string) error
+
+	// AddLabelToMessage
+	// Метод для добавления метки к сообщениям в диалоге по ID
+	AddLabelToMessage(ctx context.Context, conversationId string, messageId string, labelId string) error
+
+	// RemoveLabelFromMessage
+	// Метод для удаления метки к сообщениям в диалоге по ID
+	RemoveLabelFromMessage(ctx context.Context, conversationId string, messageId string, labelId string) error
+
+	// ReactMessage
+	// Метод для добавления/удаления реакции к сообщениям в диалоге по ID
+	ReactMessage(ctx context.Context, conversationId string, messageId string, reactionId string) error
+
+	// GetThreadMessageAll
+	// Метод для получения всех сообщений в потоке
+	GetThreadMessageAll(ctx context.Context, conversationId string, parentMessageId string) ([]conversationModel.Message, error)
+
+	// CreateThreadMessage
+	// Метод для создания и отправки сообщений в поток
+	CreateThreadMessage(ctx context.Context, conversationId string, parentMessageId string, message conversationModel.Message) (*conversationModel.Message, error)
+
+	// UpdateThreadMessage
+	// Метод для изменения сообщений в потоке
+	UpdateThreadMessage(ctx context.Context, conversationId string, parentMessageId string, messageId string, text string) (*conversationModel.Message, error)
+
+	// DeleteThreadMessageById
+	// Метод для удаления сообщений из потока по ID
+	DeleteThreadMessageById(ctx context.Context, conversationId string, parentMessageId string, messageId string) error
+
+	// AddLabelToThreadMessage
+	// Метод для добавления метки к сообщениям в потоке по ID
+	AddLabelToThreadMessage(ctx context.Context, conversationId string, parentMessageId string, messageId string, labelId string) error
+
+	// RemoveLabelFromThreadMessage
+	// Метод для удаления метки к сообщениям в потоке по ID
+	RemoveLabelFromThreadMessage(ctx context.Context, conversationId string, parentMessageId string, messageId string, labelId string) error
+
+	// ReactThreadMessage
+	// Метод для добавления/удаления реакции к сообщениям в потоке по ID
+	ReactThreadMessage(ctx context.Context, conversationId string, parentMessageId string, messageId string, reactionId string) error
 }
 
 type messageService struct {
@@ -79,6 +119,28 @@ func NewMessageService(botBaseParam model.BotBaseParam) MessageService {
 		httpClient:   &http.Client{Timeout: time.Second * 30},
 	}
 	return messageSrv
+}
+
+func (service *messageService) GetMessageAll(ctx context.Context, conversationId string) (*model.Page[conversationModel.Message], error) {
+	request, err := util.CreateHttpRequest(ctx, service.BotBaseParam, http.MethodGet, fmt.Sprintf("%s/%s/messages", conversationBasePath, conversationId), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := service.httpClient.Do(request)
+	defer util.CloseChecker(response.Body)
+	if err != nil {
+		return nil, err
+	}
+	if response.StatusCode != 200 {
+		return nil, errors.New(response.Status)
+	}
+
+	var messagePage model.Page[conversationModel.Message]
+	if err := json.NewDecoder(response.Body).Decode(&messagePage); err != nil {
+		return nil, err
+	}
+	return &messagePage, nil
 }
 
 func (service *messageService) CreateMessage(ctx context.Context, conversationId string, message conversationModel.Message) (*conversationModel.Message, error) {
@@ -105,4 +167,276 @@ func (service *messageService) CreateMessage(ctx context.Context, conversationId
 	}
 
 	return &message, err
+}
+
+func (service *messageService) UpdateMessage(ctx context.Context, conversationId string, messageId string, text string) (*conversationModel.Message, error) {
+	message := conversationModel.Message{Text: text}
+	data, err := json.Marshal(&message)
+	if err != nil {
+		return nil, err
+	}
+
+	request, err := util.CreateHttpRequest(ctx, service.BotBaseParam, http.MethodPut, fmt.Sprintf("%s/%s/messages/%s", conversationBasePath, conversationId, messageId), bytes.NewReader(data))
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := service.httpClient.Do(request)
+	defer util.CloseChecker(response.Body)
+	if err != nil {
+		return nil, err
+	}
+	if response.StatusCode != 200 {
+		return nil, errors.New(response.Status)
+	}
+
+	if err := json.NewDecoder(response.Body).Decode(&message); err != nil {
+		return nil, err
+	}
+	return &message, err
+}
+
+func (service *messageService) DeleteMessageById(ctx context.Context, conversationId string, messageId string) error {
+	request, err := util.CreateHttpRequest(ctx, service.BotBaseParam, http.MethodDelete, fmt.Sprintf("%s/%s/messages/%s", conversationBasePath, conversationId, messageId), nil)
+	if err != nil {
+		return err
+	}
+
+	response, err := service.httpClient.Do(request)
+	defer util.CloseChecker(response.Body)
+	if err != nil {
+		return err
+	}
+	if response.StatusCode != 200 {
+		return errors.New(response.Status)
+	}
+	return nil
+}
+
+func (service *messageService) PinMessage(ctx context.Context, conversationId string, messageId string) error {
+	request, err := util.CreateHttpRequest(ctx, service.BotBaseParam, http.MethodPut, fmt.Sprintf("%s/%s/messages/%s/pin", conversationBasePath, conversationId, messageId), nil)
+	if err != nil {
+		return err
+	}
+
+	response, err := service.httpClient.Do(request)
+	defer util.CloseChecker(response.Body)
+	if err != nil {
+		return err
+	}
+	if response.StatusCode != 200 {
+		return errors.New(response.Status)
+	}
+	return nil
+}
+
+func (service *messageService) UnpinMessage(ctx context.Context, conversationId string, messageId string) error {
+	request, err := util.CreateHttpRequest(ctx, service.BotBaseParam, http.MethodDelete, fmt.Sprintf("%s/%s/messages/%s/pin", conversationBasePath, conversationId, messageId), nil)
+	if err != nil {
+		return err
+	}
+
+	response, err := service.httpClient.Do(request)
+	defer util.CloseChecker(response.Body)
+	if err != nil {
+		return err
+	}
+	if response.StatusCode != 200 {
+		return errors.New(response.Status)
+	}
+	return nil
+}
+
+func (service *messageService) AddLabelToMessage(ctx context.Context, conversationId string, messageId string, labelId string) error {
+	request, err := util.CreateHttpRequest(ctx, service.BotBaseParam, http.MethodPut, fmt.Sprintf("%s/%s/messages/%s/label/%s", conversationBasePath, conversationId, messageId, labelId), nil)
+	if err != nil {
+		return err
+	}
+
+	response, err := service.httpClient.Do(request)
+	defer util.CloseChecker(response.Body)
+	if err != nil {
+		return err
+	}
+	if response.StatusCode != 200 {
+		return errors.New(response.Status)
+	}
+	return nil
+}
+
+func (service *messageService) RemoveLabelFromMessage(ctx context.Context, conversationId string, messageId string, labelId string) error {
+	request, err := util.CreateHttpRequest(ctx, service.BotBaseParam, http.MethodDelete, fmt.Sprintf("%s/%s/messages/%s/label/%s", conversationBasePath, conversationId, messageId, labelId), nil)
+	if err != nil {
+		return err
+	}
+
+	response, err := service.httpClient.Do(request)
+	defer util.CloseChecker(response.Body)
+	if err != nil {
+		return err
+	}
+	if response.StatusCode != 200 {
+		return errors.New(response.Status)
+	}
+	return nil
+}
+
+func (service *messageService) ReactMessage(ctx context.Context, conversationId string, messageId string, reactionId string) error {
+	request, err := util.CreateHttpRequest(ctx, service.BotBaseParam, http.MethodPut, fmt.Sprintf("%s/%s/messages/%s/reaction/%s", conversationBasePath, conversationId, messageId, reactionId), nil)
+	if err != nil {
+		return err
+	}
+
+	response, err := service.httpClient.Do(request)
+	defer util.CloseChecker(response.Body)
+	if err != nil {
+		return err
+	}
+	if response.StatusCode != 200 {
+		return errors.New(response.Status)
+	}
+	return nil
+}
+
+func (service *messageService) GetThreadMessageAll(ctx context.Context, conversationId string, parentMessageId string) ([]conversationModel.Message, error) {
+	request, err := util.CreateHttpRequest(ctx, service.BotBaseParam, http.MethodGet, fmt.Sprintf("%s/%s/messages/%s/thread", conversationBasePath, conversationId, parentMessageId), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := service.httpClient.Do(request)
+	defer util.CloseChecker(response.Body)
+	if err != nil {
+		return nil, err
+	}
+	if response.StatusCode != 200 {
+		return nil, errors.New(response.Status)
+	}
+
+	var messages []conversationModel.Message
+	if err := json.NewDecoder(response.Body).Decode(&messages); err != nil {
+		return nil, err
+	}
+	return messages, nil
+}
+
+func (service *messageService) CreateThreadMessage(ctx context.Context, conversationId string, parentMessageId string, message conversationModel.Message) (*conversationModel.Message, error) {
+	data, err := json.Marshal(&message)
+	if err != nil {
+		return nil, err
+	}
+
+	request, err := util.CreateHttpRequest(ctx, service.BotBaseParam, http.MethodPost, fmt.Sprintf("%s/%s/messages/%s/thread", conversationBasePath, conversationId, parentMessageId), bytes.NewReader(data))
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := service.httpClient.Do(request)
+	defer util.CloseChecker(response.Body)
+	if err != nil {
+		return nil, err
+	}
+	if response.StatusCode != 201 {
+		return nil, errors.New(response.Status)
+	}
+	if err := json.NewDecoder(response.Body).Decode(&message); err != nil {
+		return nil, err
+	}
+
+	return &message, err
+}
+
+func (service *messageService) UpdateThreadMessage(ctx context.Context, conversationId string, parentMessageId string, messageId string, text string) (*conversationModel.Message, error) {
+	message := conversationModel.Message{Text: text}
+	data, err := json.Marshal(&message)
+	if err != nil {
+		return nil, err
+	}
+
+	request, err := util.CreateHttpRequest(ctx, service.BotBaseParam, http.MethodPut, fmt.Sprintf("%s/%s/messages/%s/thread/%s", conversationBasePath, conversationId, parentMessageId, messageId), bytes.NewReader(data))
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := service.httpClient.Do(request)
+	defer util.CloseChecker(response.Body)
+	if err != nil {
+		return nil, err
+	}
+	if response.StatusCode != 200 {
+		return nil, errors.New(response.Status)
+	}
+
+	if err := json.NewDecoder(response.Body).Decode(&message); err != nil {
+		return nil, err
+	}
+	return &message, err
+}
+
+func (service *messageService) DeleteThreadMessageById(ctx context.Context, conversationId string, parentMessageId string, messageId string) error {
+	request, err := util.CreateHttpRequest(ctx, service.BotBaseParam, http.MethodDelete, fmt.Sprintf("%s/%s/messages/%s/thread/%s", conversationBasePath, conversationId, parentMessageId, messageId), nil)
+	if err != nil {
+		return err
+	}
+
+	response, err := service.httpClient.Do(request)
+	defer util.CloseChecker(response.Body)
+	if err != nil {
+		return err
+	}
+	if response.StatusCode != 200 {
+		return errors.New(response.Status)
+	}
+	return nil
+}
+
+func (service *messageService) AddLabelToThreadMessage(ctx context.Context, conversationId string, parentMessageId string, messageId string, labelId string) error {
+	request, err := util.CreateHttpRequest(ctx, service.BotBaseParam, http.MethodPut, fmt.Sprintf("%s/%s/messages/%s/thread/%s/label/%s", conversationBasePath, conversationId, parentMessageId, messageId, labelId), nil)
+	if err != nil {
+		return err
+	}
+
+	response, err := service.httpClient.Do(request)
+	defer util.CloseChecker(response.Body)
+	if err != nil {
+		return err
+	}
+	if response.StatusCode != 200 {
+		return errors.New(response.Status)
+	}
+	return nil
+}
+
+func (service *messageService) RemoveLabelFromThreadMessage(ctx context.Context, conversationId string, parentMessageId string, messageId string, labelId string) error {
+	request, err := util.CreateHttpRequest(ctx, service.BotBaseParam, http.MethodDelete, fmt.Sprintf("%s/%s/messages/%s/thread/%s/label/%s", conversationBasePath, conversationId, parentMessageId, messageId, labelId), nil)
+	if err != nil {
+		return err
+	}
+
+	response, err := service.httpClient.Do(request)
+	defer util.CloseChecker(response.Body)
+	if err != nil {
+		return err
+	}
+	if response.StatusCode != 200 {
+		return errors.New(response.Status)
+	}
+	return nil
+}
+
+func (service *messageService) ReactThreadMessage(ctx context.Context, conversationId string, parentMessageId string, messageId string, reactionId string) error {
+	request, err := util.CreateHttpRequest(ctx, service.BotBaseParam, http.MethodPut, fmt.Sprintf("%s/%s/messages/%s/thread/%s/reaction/%s", conversationBasePath, conversationId, parentMessageId, messageId, reactionId), nil)
+	if err != nil {
+		return err
+	}
+
+	response, err := service.httpClient.Do(request)
+	defer util.CloseChecker(response.Body)
+	if err != nil {
+		return err
+	}
+	if response.StatusCode != 200 {
+		return errors.New(response.Status)
+	}
+	return nil
 }
